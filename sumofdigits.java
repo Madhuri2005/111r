689e7c16f55039eaa47a71d986fd4dd96aff8d8b
Madhuri2005 <121482240+Madhuri2005@users.noreply.github.com>
2026-09-25 19:10:33 +0530

    Add files via upload

---
+++ sumofdigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sumofdigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        int m = 10;
+        int mul = 1;
+
+        while (i <= n) {
+
+        }
+
+        System.out.println();
+    }
+}
